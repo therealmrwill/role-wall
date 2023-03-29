0745a46171456c1fa2f0bed4fd1c88f59b094a2f
therealmrwill <wru5006@psu.edu>
2023-03-29 15:02:49 -0400

    Got it working!
    Co-authored-by: Keith Charlton <kcc5354@psu.edu>

--- src/RoleWall.js
+++ src/RoleWall.js
@@ -3,31 +3,90 @@ import { html, css, LitElement } from 'lit';
 export class RoleWall extends LitElement {
   static styles = css`
     :host {
-      display: block;
       padding: 25px;
       color: var(--role-wall-text-color, #000);
+      -webkit-user-select: none; /* Safari */
+      -ms-user-select: none; /* IE 10 and IE 11 */
+      user-select: none; /* Standard syntax */
+    }
+    .h1{
+      font-size: 2em;
     }
   `;
 
   static properties = {
-    header: { type: String },
-    counter: { type: Number },
+    id: { type: String },
+    locked: { type: Boolean },
+    data: { type: Array, Reflect: true },
+    shownData: { type: Array, Reflect: true },
+    lock: {type: String}
   };
 
   constructor() {
     super();
-    this.header = 'Hey there';
-    this.counter = 5;
+    this.id = "-1";
+    this.locked = true;
+    this.data = [];
+    this.shownData= [];
   }
 
-  __increment() {
-    this.counter += 1;
+  firstUpdated(changeProperties) {
+    console.log("Element loaded");
+    this._lockElement();
+    this.data = Array.from(this.children);
+    this.innerHTML = "";
+    this.shownData.push(html`<h1 class="h1">This content is locked</h1>`);
   }
+
+  _lockElement(){
+    if(this.locked) return;
+
+    this.locked = true;
+    console.log("Attempting to lock element");
+    this.shownData = [];
+    this.shownData.push(html`<h1 class="h1">This content is locked</h1>`);
+  }
+
+  _unlockElement(){
+    if(this.locked == false) return;
+
+    this.locked = false;
+    console.log("Attempting to unlock element");
+    this.shownData = this.data;
+  }
+  
+  _addLockedContent(content){
+    content.push("<h1>This content is locked</h1>");
+  
+  }
+
+
+  //render 
+
+  //return html' 
+    //$(content)
+
 
   render() {
-    return html`
-      <h2>${this.header} Nr. ${this.counter}!</h2>
-      <button @click=${this.__increment}>increment</button>
-    `;
+    var tempText = this.locked ? "Unlock Content" : "Lock Content";
+
+    var content = [];
+
+    this.shownData.forEach(element => {
+        content.push(element);
+    });
+
+      return html`
+      ${content}
+      <div>
+        <button @click=${this.locked ? this._unlockElement : this._lockElement} style="-webkit-user-select: none; -ms-user-select: none; 
+        user-select: none;"> ${tempText} </button>
+      </div>
+      
+    `
+
+    
+
+    
   }
 }
